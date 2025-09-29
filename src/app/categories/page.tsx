2f202c69aae80f9f659221aa5197e710d061d350
Glenn-Rhee <glennviktor5@gmail.com>
2025-09-29 11:23:11 +0700

    feat: added categories icon

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,3 +1,52 @@
+import BackBtn from "@/components/BackBtn";
+import CardAnalysis from "@/components/CardAnalysis";
+import Drawer from "@/components/Drawer";
+import Header from "@/components/Header";
+import LogoutBtn from "@/components/LogoutBtn";
+import Link from "next/link";
+import { Suspense } from "react";
+
+interface Icon {
+  icon: string;
+  label: string;
+}
+
+const icons: Icon[] = [
+  { icon: "restaurant-line", label: "Food" },
+  { icon: "bus-line", label: "Transport" },
+  { icon: "capsule-line", label: "Medicine" },
+  { icon: "shopping-basket-line", label: "Groceries" },
+  { icon: "key-line", label: "Rent" },
+  { icon: "gift-line", label: "Gifts" },
+  { icon: "coins-line", label: "Savings" },
+  { icon: "ticket-line", label: "Entertainment" },
+];
+
 export default function CategoriesPage() {
-  return <h1>Categories Page</h1>;
+  return (
+    <Suspense>
+      <Header>
+        <BackBtn />
+        <span className="text-xl font-bold text-darkmode-green-bar">
+          Categories
+        </span>
+        <LogoutBtn />
+      </Header>
+      <CardAnalysis />
+      <Drawer className="justify-start overflow-y-auto items-start px-8 pt-4 max-h-[33rem]">
+        <div className="grid grid-cols-3 mb-4 gap-x-4 gap-y-8 w-full">
+          {icons.map((icon, i) => (
+            <Link href={"/categories/" + icon.label.toLowerCase()} key={i}>
+              <div className="aspect-square bg-lightblue-btn transition-colors duration-200 hover:bg-oceanblue-btn flex items-center justify-center px-8 py-4 rounded-md">
+                <i className={`ri-${icon.icon} text-4xl text-white`}></i>
+              </div>
+              <span className="text-center block text-lettersIcon font-semibold">
+                {icon.label}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </Drawer>
+    </Suspense>
+  );
 }
