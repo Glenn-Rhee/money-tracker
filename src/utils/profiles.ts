a36d8e77dfc741fa78a73ec9aa922b33e23e5430
Glenn-Rhee <glennviktor5@gmail.com>
2025-10-05 15:35:57 +0700

    feat: added profile page

---
+++ src/utils/profiles.ts
@@ -0,0 +1,17 @@
+export const profiles = [
+  {
+    href: "/profile/edit",
+    icon: "user-line",
+    title: "Edit Profile",
+  },
+  {
+    href: "/profile/security",
+    icon: "shield-check-line",
+    title: "Security",
+  },
+  {
+    href: "/profile/setting",
+    icon: "settings-line",
+    title: "Setting",
+  },
+];
